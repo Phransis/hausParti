1517c7134dace76ef9b5278fe92884027710a09e
Duo Francis <duofrancis11@gmil.com>
2024-06-03 21:41:43 +0000

    keep on pushing

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,41 +1,34 @@
 import React from "react";
 
 function Footer() {
-  return <>
-      <div className='footer '>
-        <div className='column1'>
-        <div className="left">
-          <a href="/">
-            <span className="haus">haus</span>
-            <span className="parti">
-              <i>Parti.</i>
-            </span>
-          </a>
-          <div className="footer1">
+  return (
+    <>
+      <div className="footer mx-auto pt-10 ">
+        <div className="column1">
+          <div className=" ">
+            <a href="/">
+              <span className="haus">haus</span>
+              <span className="parti">
+                <i>Parti.</i>
+              </span>
+            </a>
+          </div>
+          <div className="">
             <ul>
-              <a href="blog">
-                Blog
-              </a>
-              <a href="events">
-                Events
-              </a>
-              <a href="about">
-                About
-              </a>
-              <a href="blog">
-                Blog
-              </a>
-              <a href="blog">
-                Blog
-              </a>
+              <a href="blog">Blog</a>
+              <a href="events">Events</a>
+              <a href="about">About</a>
+              <a href="blog">Blog</a>
+              <a href="blog">Blog</a>
             </ul>
           </div>
         </div>
-        </div>
-        <div className='column2'>Location</div>
-        <div className='column3'>Subscribe to our newsletters</div>
-    </div>
-  </>;
+        <div className="column2">Location</div>
+        <div className="column3">Subscribe to our newsletters</div>
+        <div className="column4">Year</div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import Outside from "../assets/outside.png";
 function HomePage() {
   return (
     <>
-      <div className="">
+      <div className=""><hr></hr>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
         quidem ex doloremque saepe magnam magni, eum et nesciunt dolore pariatur
         possimus. Quam laboriosam possimus, ad molestias veniam at ea expedita?
